e59ead1a7c840e2d2c629462042f7270724b4045
siddhiagrwl <143806538+siddhiagrwl@users.noreply.github.com>
2023-10-16 23:17:31 +0530

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,96 @@
+let resumeData = {
+
+    "name": "Siddhi Agrawal",
+    "role": "Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/siddhi-agrawal-0b5a89283?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BCmq8BQIZR2S%2BNOk3Vse4pw%3D%3D",
+    "roleDescription": "I am curretnly learning MERN Full stack and I am brushing up my skills in Python domain.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/siddhi-agrawal-0b5a89283?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BCmq8BQIZR2S%2BNOk3Vse4pw%3D%3D",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/siddhiagrwl",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The Vellore Institute of Technology and pursuing my B.Tech in Information Technology. I am currently learning Full Stack Web Development and brushing up my skills Python. I am also aiming to delve into world of Ai and Ml in near future.",
+    "address":"India",
+    "website":"siddhiagrawal010@gmail.com",
+    "education":[
+      {
+        "UniversityName":"The Vellore Insitute of Technology",
+        "specialization":"Information Technology",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2025"
+        
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"F.E.P.S.I",
+        "specialization":"Core Committee Member",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2023",
+        "Achievements":"Successfully conducted Startup Street 7.0 event in GRAVITAS'22(tech fest of VIT)."
+      },
+    ],
+    // "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"Expressjs"
+      },
+      {
+        "skillname":"SQL"
+      },
+      {
+        "skillname":"Python"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Project1",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80",
+        "url":"https://arnav240703.github.io/html_css_0026/"
+      },
+      {
+        "name":"Project2",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      },
+      {
+        "name":"Project3",
+        "description":"mobileapp",  
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      },
+      {
+        "name":"Project4",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      }
+    ],
+    "testimonials":[
+      
+      {
+        "description":"Your AI, ML skills are impressive and consistently deliver outstanding results.",
+        "name":"Meet Movaliya"
+      }
+    ]
+  }
+  
+  export default resumeData
